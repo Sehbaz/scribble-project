72f614703fb12fec23c7ae79a34e576188120e88
Sehbaz Rafik <sehbazm@gmail.com>
2021-02-10 02:38:05 +0000

    sign in model added

--- scripts/index.js
+++ scripts/index.js
@@ -1,25 +1,48 @@
-// Get the modal
-var modal = document.getElementById("myModal");
+//Sign Up model
+var signupmodal = document.getElementById("signupModal");
 
 // Get the button that opens the modal
-var btn = document.getElementById("su");
+var btnsu = document.getElementById("su");
 
 // Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
+var signupspan = document.getElementsByClassName("closesignup")[0];
 
-// When the user clicks the button, open the modal 
-btn.onclick = function () {
-    modal.style.display = "block";
+// When the user clicks on the button, open the modal
+btnsu.onclick = function () {
+    signupmodal.style.display = "block";
 }
 
 // When the user clicks on <span> (x), close the modal
-span.onclick = function () {
-    modal.style.display = "none";
+signupspan.onclick = function () {
+    signupmodal.style.display = "none";
 }
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function (event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
+    if (event.target == signupmodal) {
+        signupmodal.style.display = "none";
+    }
+}
+
+
+// Sign In model
+var singinmodal = document.getElementById("signinModal");
+// Get the button that opens the modal
+var btnsi = document.getElementById("si");
+// Get the <span> element that closes the modal
+var signinspan = document.getElementsByClassName("closesignin")[0];
+// When the user clicks on the button, open the modal
+btnsi.onclick = function () {
+    signinModal.style.display = "block";
+}
+// When the user clicks on <span> (x), close the modal
+signinspan.onclick = function () {
+    signinModal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+    if (event.target == signinModal) {
+        signinModal.style.display = "none";
     }
 }
